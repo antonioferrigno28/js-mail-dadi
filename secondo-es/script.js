c90eeedaf58cdde53d26ff2371bd54c34e6c313d
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-17 17:49:32 +0200

    DONE!

--- secondo-es/script.js
+++ secondo-es/script.js
@@ -19,17 +19,41 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 //ELABORAZIONE
 
 //Dichiarazione delle due variabili (costanti in questo caso) che rappresentano il dado dell'utente ed il dado del computer
+const userNumber = Math.floor(Math.random() * 6) + 1;
+const computerNumber = Math.floor(Math.random() * 6) + 1;
+
+//INTRO
+alert(
+  "Domani devi partire per la Puglia con tua moglie.\nStai passeggiando ed in un momento di noia, con i soldi del viaggio in tasca, vedi un BoolBet e decidi di tentare la sorte."
+);
+
+alert(
+  "Benvenuto al BoolBet di Palmanova, qui NON si giocano i dadi all'americana (quelli rossi) ma alla francese (quelli bianchi).\nIl gioco d'azzardo può causare dipendenza patologica, ricordati di giocare in base alle tue possibilità"
+);
+
+alert("Sono 200€ ogni mano, se accetti continua pure, altrimenti esci");
 
 //OUTPUT LANCIO DADI
+alert("Il numero del computer è" + " " + computerNumber);
+alert("Il numero dell'utente è" + " " + userNumber);
 
 //SE il numero del computer e dell'utente sono uguali
-
-//Display messaggio di pareggio
-
+if (computerNumber === userNumber) {
+  //Display messaggio di pareggio
+  alert("C'è stato un pareggio, i 200€ che hai giocato sono ancora tuoi");
+}
 //ALTRIMENTI SE il numero del computer è minore del numero dell'utente
-
-//Display messaggio di vittoria
-
-//ALTRIMENTI
-
-//Display messaggio di sconfitta
+else if (computerNumber < userNumber) {
+  //Display messaggio di vittoria
+  alert("Hai vinto!");
+  alert(
+    "Torno subito a casa, do la notizia a mia moglie ed allunghiamo il viaggio di qualche giorno"
+  );
+  //ALTRIMENTI
+} else {
+  //Display messaggio di sconfitta
+  alert("Hai perso");
+  alert(
+    "Torno a casa facendo il giro lungo, vado da mia moglie che mi guarda già entusiasta per il viaggio di domani e le confesso di aver perso tutti i soldi del viaggio...\nNon si parte più, sarà meglio chiamare l'hotel e disdire."
+  );
+}
