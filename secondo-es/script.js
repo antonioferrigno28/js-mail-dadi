9a9022300af05461dbbc5d43fafefe3ada9c7d34
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-17 16:40:17 +0200

    Es 2 Pseudocode ITA

---
+++ secondo-es/script.js
@@ -0,0 +1,35 @@
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+/* Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+3. si ma noi cosa vogliamo fare?
+4. torniamo a scrivere in italiano
+5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ESERCIZIO 2:DADI
+
+//RACCOLTA DATI
+//Non dobbiamo raccogliere alcun dato dall'utente
+
+//ELABORAZIONE
+
+//Dichiarazione delle due variabili (costanti in questo caso) che rappresentano il dado dell'utente ed il dado del computer
+
+//OUTPUT LANCIO DADI
+
+//SE il numero del computer e dell'utente sono uguali
+
+//Display messaggio di pareggio
+
+//ALTRIMENTI SE il numero del computer è minore del numero dell'utente
+
+//Display messaggio di vittoria
+
+//ALTRIMENTI
+
+//Display messaggio di sconfitta
